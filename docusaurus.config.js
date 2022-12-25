b3ecc92ed03812e7a7bec173a855fcec3c133462
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2022-12-25 08:19:05 +0000

    Init upload

---
+++ docusaurus.config.js
@@ -0,0 +1,125 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'NetPlus使用手册',
+  tagline: '啊哈，你没看错，这是NetPlus文档！',
+  url: 'https://docs.natgo.cn',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'vlssu', // Usually your GitHub org/user name.
+  projectName: 'docs-netgo', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', // 把文档放在网站根部
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/vlssu/docs-netgo/tree/master/docs/',
+        },
+        blog: false,
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/vlssu/docs-netgo/tree/master/blog/',
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'NetPLUS',
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg',
+        // },
+        items: [
+          // {
+          //   type: 'doc',
+          //   docId: 'README',
+          //   position: 'left',
+          //   label: '文档',
+          // },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/vlssu/docs-netgo',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '社区',
+            items: [
+              {
+                label: 'QQ群',
+                href: 'https://jq.qq.com/?_wv=1027&k=xniHysmW',
+              },
+              {
+                label: 'QQ频道',
+                href: 'https://pd.qq.com/s/2ruif327f',
+              },
+            ],
+          },
+          {
+            title: '友情链接',
+            items: [
+              {
+                label: '游端云',
+                href: 'https://cvmidc.cn/',
+              },
+              {
+                label: 'VLssuSkin - 皮肤站',
+                href: 'https://skin.vlssu.com',
+              },
+              {
+                label: 'VLssu公益服',
+                href: 'https://vlssu.cn/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} NetGO, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
